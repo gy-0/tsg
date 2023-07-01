12261ec2b3be3239a3d64727bbaa1d7b85b684f0
gy-0 <41229059+gy-0@users.noreply.github.com>
2023-07-01 17:51:47 +0800

    Create Aldente.js

---
+++ Aldente.js
@@ -0,0 +1,10 @@
+{
+    "success": true,
+    "response": {
+        "product_id": "647568",
+        "activation_id": "admin",
+        "type": "personal",
+        "expires": 1,
+        "expiry_date": 1999999999999
+    }
+}
