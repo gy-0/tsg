3fbb50155e61fa9f2e9feb4f76223b34d1dab873
gy-0 <41229059+gy-0@users.noreply.github.com>
2023-07-01 18:11:41 +0800

    Update Aldente.js

--- Aldente.js
+++ Aldente.js
@@ -1,10 +1,1 @@
-{
-    "success": true,
-    "response": {
-        "product_id": "647568",
-        "activation_id": "admin",
-        "type": "personal",
-        "expires": 1,
-        "expiry_date": 1999999999999
-    }
-}
+{"success": true,"response": {product_id": "647568","activation_id": "admin","type": "personal","expires": 1,"expiry_date": 1999999999999}}
